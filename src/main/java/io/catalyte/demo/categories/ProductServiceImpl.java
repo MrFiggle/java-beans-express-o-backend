0a417fe319260dbb39e34e53655f5cf26c4aeb3d
Taylor Brooks <tbrooks@catalyte.io>
2024-05-20 17:06:36 -0400

    remove unneeded import

--- src/main/java/io/catalyte/demo/categories/ProductServiceImpl.java
+++ src/main/java/io/catalyte/demo/categories/ProductServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
